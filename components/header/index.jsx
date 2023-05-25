2735de2a98ea3f3936a28aebbf8497197bb44543
Mariana Dias Pereira <marianamkt6@gmail.com>
2023-05-25 11:36:32 -0300

    logo

---
+++ components/header/index.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import link from "next/link";
+import logo from 'public/images/logo.png';
+import Styles from "./header.module.scss"
+
+
+
+const Header = () =>{
+
+return <div className={Styles.container}> 
+
+
+
+
+<div className={Styles.logo}>
+
+<Image src={logo} />
+
+
+</div>
+
+<div className={Styles.menu}>
+   <a href="/"> Home </a>
+    <a href="/"> O que fazemos </a> 
+    <a href="/"> Cases </a>
+   
+
+</div>
+
+
+<div className={Styles.action}>
+
+<button className={Styles.button}>Fale conosco</button>
+
+</div>
+
+ </div>
+
+};
+
+export default Header
---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+
+
+import Header from "../components/Header"
+import styles from './Home.module.scss'
+
+
+export default function Home() {
+  return (
+   <div className={styles.container}>
+
+          <Header />
+   </div>
+  )
+}
